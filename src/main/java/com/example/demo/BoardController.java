c288d8f233ad5b4d8e50184ff534f49373f826a1
vosw1 <vosw1@naver.com>
2024-02-24 14:55:20 +0900

    header 추가 및 기업 main 페이지 추가

--- src/main/java/com/example/demo/BoardController.java
+++ src/main/java/com/example/demo/BoardController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class BoardController {
 
     @GetMapping("/")
-    public String index(){
-
-        return "/";
+    public String main() {
+        return "index1"; // index.mustache 템플릿 파일을 반환
     }
 }
