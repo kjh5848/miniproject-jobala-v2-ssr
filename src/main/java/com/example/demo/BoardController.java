6235c421df92dd69154a82e97f2fba7912c0df0f
vosw1 <you@example.com>
2024-02-26 18:54:14 +0900

    디자인 수정 by 부트스트랩

--- src/main/java/com/example/demo/BoardController.java
+++ src/main/java/com/example/demo/BoardController.java
@@ -6,8 +6,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    public String main() {
-        return "index1"; // index.mustache 템플릿 파일을 반환
+    @GetMapping("/comp/mainForm")
+    public String compMain() {
+        return "/comp/mainForm"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/comp/Mypage")
+    public String compMypage() {
+        return "/comp/MypageForm"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/comp/supportManagement")
+    public String supportManagement() {
+        return "/comp/supportManagement"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/comp/employmentAddForm")
+    public String employmentAddForm() {
+        return "/comp/employmentAddForm"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/comp/boardForm")
+    public String boardForm() {
+        return "/comp/boardForm"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/comp/logout")
+    public String logout() {
+        return "compMain"; // index.mustache 템플릿 파일을 반환
     }
 }
---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/")
+    public String main() {
+        return "index"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/user/mypage")
+    public String userMypage() {
+        return "/user/mypage"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/user/resume")
+    public String resume() {
+        return "/user/resume"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/user/supportManagement")
+    public String supportManagement() {
+        return "/user/supportManagement"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/user/clippedCompany")
+    public String clippedCompany() {
+        return "/user/clippedCompany"; // index.mustache 템플릿 파일을 반환
+    }
+
+    @GetMapping("/user/boardForm")
+    public String boardForm() {
+        return "/user/boardForm"; // index.mustache 템플릿 파일을 반환
+    }
+
+}
