f444b988686e5bd9220a5297bb10136fd84307ee
vosw1 <vosw1@naver.com>
2024-02-28 09:05:55 +0900

    footer 수정

--- src/main/java/com/example/demo/BoardController.java
+++ src/main/java/com/example/demo/BoardController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class BoardController {
+
     @GetMapping("/board/boardForm")
     public String boardForm() {
-        return "board/boardForm";
+        return "/board/boardForm";
     }
 
     @GetMapping("/board/boardDetail")
