df5a84ebb43a1b0f14ecd45db0cf68b2c7846533
ChanHyoek Park <1625787@donga.ac.kr>
2024-03-01 14:27:44 +0900

    테이블 필드값 넣기 완료

--- src/main/java/com/example/jobala/board/Board.java
+++ src/main/java/com/example/jobala/board/Board.java
@@ -11,4 +11,9 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    private String title;
+    private String content;
+
+    private int userId;
 }
