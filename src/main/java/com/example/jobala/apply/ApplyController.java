c59bc591f69811012e58e57c029a249bd99feaa0
hillview0303 <j910219@gmail.com>
2024-03-02 19:42:56 +0900

    제안 DTO 완료

--- src/main/java/com/example/jobala/apply/ApplyController.java
+++ src/main/java/com/example/jobala/apply/ApplyController.java
@@ -1,5 +1,7 @@
 package com.example.jobala.apply;
 
+import com.example.jobala.resume.ResumeRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class ApplyController {
+
+    private final HttpSession session;
+
+    private final ApplyRepository applyRepository;
+    private final ResumeRepository resumeRepository;
 
     @GetMapping("/guest/applyStatusForm")
     public String applyStatusForm() {
--- src/main/java/com/example/jobala/apply/ApplyResponse.java
+++ src/main/java/com/example/jobala/apply/ApplyResponse.java
@@ -11,7 +11,16 @@ public class ApplyResponse {
         private String title; // 이력서 제목
         private String edu; // 학력
         private String address; // 주소
-        private String status; // 지원자 상태
+        private String state; // 지원자 합격 불합격 상태
         private LocalDateTime createdAt; // 지원일시
+    }
+
+    @Data
+    public static class positionDetailDTO {
+        private String name; // 지원자 이름
+        private String resumeTitle; // 이력서 제목
+        private String jobopenTitle; // 공고 제목
+        private String status; // 지원자 제안중 상태
+
     }
 }
