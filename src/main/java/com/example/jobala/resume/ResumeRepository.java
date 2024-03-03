b189a9848a1d4f04a5264fca26a4217d6016f04e
kjh5848 <kjh5848@gmail.com>
2024-03-03 21:24:48 +0900

    이메일 추가 및 로그인 벨류값 설정 완료

--- src/main/java/com/example/jobala/resume/ResumeRepository.java
+++ src/main/java/com/example/jobala/resume/ResumeRepository.java
@@ -13,10 +13,10 @@ public class ResumeRepository {
 
     public Resume findByUserId(Integer userId) {
         String q = """
-            SELECT r FROM Resume r WHERE r.id = (
-                SELECT a.resumeId FROM Apply a WHERE a.userId = :userId
-            )
-            """;
+                SELECT r FROM Resume r WHERE r.id = (
+                    SELECT a.resumeId FROM Apply a WHERE a.userId = :userId
+                )
+                """;
         return em.createQuery(q, Resume.class)
                 .setParameter("userId", userId)
                 .getSingleResult();
@@ -32,7 +32,7 @@ public class ResumeRepository {
 
     @Transactional
     public void save(ResumeRequest.SaveDTO resumeSaveDTO, int userId) {
-        Query query = entityManager.createNativeQuery("insert into resume_tb(user_id, resume_title, hope_job, career, license, content, edu, created_at) values (?,?,?,?,?,?,?,now())");
+        Query query = em.createNativeQuery("insert into resume_tb(user_id, resume_title, hope_job, career, license, content, edu, created_at) values (?,?,?,?,?,?,?,now())");
         System.out.println(resumeSaveDTO);
         query.setParameter(1, userId);
         query.setParameter(2, resumeSaveDTO.getResumeTitle());
