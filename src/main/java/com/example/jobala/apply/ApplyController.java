b3f343bf91c37edcd0eb74aa894b093fe0477b41
hillview0303 <j910219@gmail.com>
2024-03-05 00:43:50 +0900

    mustache 디자인 구조 변경 완료

--- src/main/java/com/example/jobala/apply/ApplyController.java
+++ src/main/java/com/example/jobala/apply/ApplyController.java
@@ -1,9 +1,6 @@
 package com.example.jobala.apply;
 
-import com.example.jobala._core.util.ApiUtil;
-import com.example.jobala.jobopen.Jobopen;
-import com.example.jobala.jobopen.JobopenRepository;
-import com.example.jobala.resume.Resume;
+
 import com.example.jobala.resume.ResumeRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -20,53 +17,53 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-//@Controller
-//@RequiredArgsConstructor
-//public class ApplyController {
-//
-//    private final HttpSession session;
-//
-//    private final ApplyRepository applyRepository;
-//    private final ResumeRepository resumeRepository;
-
-    // 이력서 상세페이지에서 지원자 목록을 가져와서 카드 생성
-@RestController
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/api")
 public class ApplyController {
+
+    private final HttpSession session;
 
     private final ApplyRepository applyRepository;
     private final ResumeRepository resumeRepository;
-    private final JobopenRepository jobopenRepository;
 
-    @GetMapping("/applicants/{jobopenId}")
-    public ResponseEntity<List<ApplyResponse.CardDetailDTO>> getApplicantsWithResumesByJobOpenId(@PathVariable Integer jobopenId) {
-        List<Apply> applies = applyRepository.findByJobOpenId(jobopenId);
-        List<ApplyResponse.CardDetailDTO> cardDetails = new ArrayList<>();
-
-        // Jobopen 객체를 가져옴
-        Jobopen jobopen = jobopenRepository.findById(jobopenId);
-        if (jobopen == null) {
-            return ResponseEntity.notFound().build(); // 해당 jobopen이 없는 경우 404 에러 반환
-        }
-
-        for (Apply apply : applies) {
-            Resume resume = resumeRepository.findByResumeId(apply.getResumeId());
-            if (resume != null) {
-                ApplyResponse.CardDetailDTO cardDetail = new ApplyResponse.CardDetailDTO();
-                cardDetail.setName(resume.getName());
-                cardDetail.setJobopenTitle(jobopen.getJobopenTitle());
-                cardDetail.setResumeTitle(resume.getResumeTitle());
-                cardDetail.setEdu(resume.getEdu());
-                cardDetail.setState(apply.getState());
-                cardDetail.setCreatedAt(apply.getCreatedAt());
-                cardDetails.add(cardDetail);
-            }
-        }
-
-        return ResponseEntity.ok(cardDetails);
-    }
+    // 이력서 상세페이지에서 지원자 목록을 가져와서 카드 생성
+//@RestController
+//@Controller
+//@RequiredArgsConstructor
+//@RequestMapping("/api")
+//public class ApplyController {
+//
+//    private final ApplyRepository applyRepository;
+//    private final ResumeRepository resumeRepository;
+//    private final JobopenRepository jobopenRepository;
+//
+//    @GetMapping("/applicants/{jobopenId}")
+//    public ResponseEntity<List<ApplyResponse.CardDetailDTO>> getApplicantsWithResumesByJobOpenId(@PathVariable Integer jobopenId) {
+//        List<Apply> applies = applyRepository.findByJobOpenId(jobopenId);
+//        List<ApplyResponse.CardDetailDTO> cardDetails = new ArrayList<>();
+//
+//        // Jobopen 객체를 가져옴
+//        Jobopen jobopen = jobopenRepository.findById(jobopenId);
+//        if (jobopen == null) {
+//            return ResponseEntity.notFound().build(); // 해당 jobopen이 없는 경우 404 에러 반환
+//        }
+//
+//        for (Apply apply : applies) {
+//            Resume resume = resumeRepository.findByResumeId(apply.getResumeId());
+//            if (resume != null) {
+//                ApplyResponse.CardDetailDTO cardDetail = new ApplyResponse.CardDetailDTO();
+//                cardDetail.setName(resume.getName());
+//                cardDetail.setJobopenTitle(jobopen.getJobopenTitle());
+//                cardDetail.setResumeTitle(resume.getResumeTitle());
+//                cardDetail.setEdu(resume.getEdu());
+//                cardDetail.setState(apply.getState());
+//                cardDetail.setCreatedAt(apply.getCreatedAt());
+//                cardDetails.add(cardDetail);
+//            }
+//        }
+//
+//        return ResponseEntity.ok(cardDetails);
+//    }
 
     @GetMapping("/applyStatusForm")
     public String applyStatusForm() {
