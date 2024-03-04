e1ee4d438a53684675b1c86d7583987467724770
yeeun0118s <yeeun0118s@naver.com>
2024-03-04 10:18:17 +0900

    회원가입v2/더미수정완료

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -2,7 +2,7 @@
 insert into user_tb(name, username, email, password, address, phone, role, created_at)
 values ('박선규', 'cos', 'cos@nate.com', '1234', '부산광역시 금정구', '01012345678', 0, now());
 insert into user_tb(comp_num, ceo, compname, address, username, email, password, name, phone, role, created_at)
-values ( '5678', '홍길동', '활빈당컴', '부산광역시 전포', 'ssar', 'ssar@nate.com', '1234', '홍길동', '01011112222', 1, now());
+values ( '5678', '홍길동', '활빈당컴', '부산광역시 전포', 'love', 'love@nate.com', '1234', '홍길동', '01011112222', 1, now());
 
 
 -- 이력서 더미
