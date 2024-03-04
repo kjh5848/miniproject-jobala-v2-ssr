07cb1a5d382762e8d3e83c0c9a75ef95c2ab238e
ChanHyoek Park <1625787@donga.ac.kr>
2024-03-04 20:06:39 +0900

    이력서 기본ì User 보 불러오기

--- src/main/java/com/example/jobala/resume/ResumeController.java
+++ src/main/java/com/example/jobala/resume/ResumeController.java
@@ -1,6 +1,8 @@
 package com.example.jobala.resume;
 
+import com.example.jobala._user.User;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ResumeController {
 
     private final ResumeRepository resumeRepository;
+    private final HttpSession session;
 
     @GetMapping("/guest/resume/writeForm")
     public String writeForm() {
@@ -37,6 +40,10 @@ public class ResumeController {
     @GetMapping("/guest/resume/{id}")
     public String detailForm(@PathVariable Integer id, HttpServletRequest request) {
         Resume resume = resumeRepository.findById(id);
+        User user = (User) session.getAttribute("sessionUser");
+        System.out.println("sessionUser = " + user);
+        
+        request.setAttribute("user", user);
         request.setAttribute("resume", resume);
         return "/guest/resume/detailForm";
     }
