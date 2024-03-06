bf2db63b0a1dc67b0aeee3f981f99067630c24c2
yeeun0118s <yeeun0118s@naver.com>
2024-03-06 17:51:17 +0900

    스크랩 구Ajax로 구현중

--- src/main/java/com/example/jobala/scrap/ScrapController.java
+++ src/main/java/com/example/jobala/scrap/ScrapController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,6 +20,11 @@ public class ScrapController {
         List<Resume> resumeList = scrapRepository.findResumeAll();
         req.setAttribute("resumeList", resumeList);
         return "/comp/_myPage/scrapForm";
+    }
+
+    @GetMapping("/comp/scrap")
+    public void scrapResume(@RequestParam Long resumeId){
+
     }
 
     @GetMapping("/guest/scrapForm")
