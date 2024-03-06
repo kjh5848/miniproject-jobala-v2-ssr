0f724e2a64af24166602ddc66b5b8e9747d8c5bf
hillview0303 <ujin0105@gmail.com>
2024-03-06 09:35:43 +0900

    guestDTO생성

--- src/main/java/com/example/jobala/guest/GuestResponse.java
+++ src/main/java/com/example/jobala/guest/GuestResponse.java
@@ -1,5 +1,28 @@
 package com.example.jobala.guest;
 
 
+import lombok.Data;
+
+import java.sql.Date;
+
 public class GuestResponse {
+    @Data
+    public static class GuestDTO {
+        private Integer id;
+        private String jobopenTitle;
+        private String compLocation;
+        private String career;
+        private String edu;
+        private Date endTime;
+
+
+        public GuestDTO(Integer id, String jobopenTitle, String compLocation, String career, String edu, Date endTime) {
+            this.id = id;
+            this.jobopenTitle = jobopenTitle;
+            this.compLocation = compLocation;
+            this.career = career;
+            this.edu = edu;
+            this.endTime = endTime;
+        }
+    }
 }
