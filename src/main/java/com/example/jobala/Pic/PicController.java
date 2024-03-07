2ae1d915802092d805eb85a1b8db6f3e425659e7
vosw1 <vosw1@naver.com>
2024-03-07 10:04:01 +0900

    이력서 사진 저장 및 제출 완료

--- src/main/java/com/example/jobala/Pic/PicController.java
+++ src/main/java/com/example/jobala/Pic/PicController.java
@@ -1,11 +1,15 @@
 package com.example.jobala.Pic;
 
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -19,14 +23,16 @@ public class PicController {
     private final String uploadDir = "./image/"; // 파일 업로드 디렉토리
 
     @PostMapping("/upload")
-    public void upload(@RequestParam("imgFile") MultipartFile imgFile, HttpServletResponse response) {
+    public ResponseEntity<String> upload(@RequestParam("imgFile") MultipartFile imgFile) {
         String filename = imgFile.getOriginalFilename(); // 업로드된 파일명 가져오기
-        Path file = Paths.get(uploadDir + filename); // 파일 경로 생성
+        Path filePath = Paths.get(uploadDir + filename); // 파일 경로 생성
 
-        try (OutputStream os = new FileOutputStream(file.toFile())) {
+        try (OutputStream os = new FileOutputStream(filePath.toFile())) {
             os.write(imgFile.getBytes()); // 파일 쓰기
+            return ResponseEntity.ok().body("이미지 업로드가 성공하였습니다.");
         } catch (IOException e) {
             e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("이미지 업로드 중 오류가 발생하였습니다.");
         }
     }
 }
