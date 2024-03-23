c407b26d5aa4ea7bf6c7472f6d3cf8c89f4e6b50
vosw1 <vosw1@naver.com>
2024-03-23 12:06:25 +0900

    게시글 상세보기 Service까지 구현

--- src/test/java/com/example/jobala/board/BoardJPARepositoryTest.java
+++ src/test/java/com/example/jobala/board/BoardJPARepositoryTest.java
@@ -1,7 +1,38 @@
 package com.example.jobala.board;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class BoardJPARepositoryTest {
+
+    @Autowired
+    private BoardJPARepository boardJPARepository;
+
+    @Autowired
+    private EntityManager em;
+
+    //findById
+    @Test
+    public void findById_test(){
+    //given
+        int id = 1;
+    //when
+        Optional<Board> boardOP = boardJPARepository.findById(id);
+
+        if(boardOP.isPresent()){
+            Board board = boardOP.get();
+            System.out.println("findById_test : "+board.getTitle());
+        }
+
+
+    //then
+    }
 
 }
