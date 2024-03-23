c52c51cf93e15f210b4e566c1a6b346b23fd09eb
vosw1 <vosw1@naver.com>
2024-03-23 12:06:25 +0900

    guest profile에 {{imgFilename}} 하려다가 터짐.. 테이블에 imgFilename 추가한게 다

--- src/main/java/com/example/jobala/Pic/PicJPARepository.java
+++ src/main/java/com/example/jobala/Pic/PicJPARepository.java
@@ -1,6 +1,6 @@
-package com.example.jobala.Pic;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PicJPARepository extends JpaRepository<Pic, Integer> {
-}
+//package com.example.jobala.Pic;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface PicJPARepository extends JpaRepository<Pic, Integer> {
+//}
