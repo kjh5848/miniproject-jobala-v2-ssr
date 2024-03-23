57a490bb1af2f965034622eae72753e533cd39d0
hillview0303 <ujin0105@gmail.com>
2024-03-23 21:51:15 +0900

    지원 저장 변환 완료

--- src/test/java/com/example/jobala/apply/ApplyJPARepositoryTest.java
+++ src/test/java/com/example/jobala/apply/ApplyJPARepositoryTest.java
@@ -1,7 +1,27 @@
 package com.example.jobala.apply;
 
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class ApplyJPARepositoryTest {
 
+    @Autowired
+    private ApplyJPARepository applyJPARepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+     public void _test(){
+         // given
+
+         // when
+
+         // then
+     }
 }
