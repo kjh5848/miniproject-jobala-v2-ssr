4755b5d017ff7b99b48db8a3da685475d0aa82bc
test231212 <j910219@gmail.com>
2024-03-29 17:54:23 +0900

    user, comp 서비스로 전환

--- src/main/java/com/example/jobala/comp/CompController.java
+++ src/main/java/com/example/jobala/comp/CompController.java
@@ -34,64 +34,48 @@ public class CompController {
     private final ResumeJPARepository resumeJPARepository;
 
 
-    // TODO: 서비스 만들기
     @GetMapping("/comp/resumeSearch")
     public String jobopenSearch(HttpServletRequest req, @RequestParam(value = "skills", defaultValue = "") String skills, CompResponse.SearchDTO resDTO) {
-        List<ResumeResponse.ListDTO> resumeList = compQueryRepository.findAll(skills, resDTO);
+        List<ResumeResponse.ListDTO> resumeList = compService.searchResumes(skills, resDTO);
         req.setAttribute("resumeList", resumeList);
         return "comp/scoutList";
     }
 
-    // TODO: 서비스 만들기
     @GetMapping("/comp/scoutList")
     public String scoutList(HttpServletRequest req) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-        List<ResumeResponse.ListDTO> resumeList = compQueryRepository.findResumeAll();
+        List<ResumeResponse.ListDTO> resumeList = compService.listAllResumes();
         req.setAttribute("resumeList", resumeList);
         return "comp/scoutList";
     }
 
-    // TODO: 서비스 만들기
     @GetMapping("/comp/scoutList/{id}")
     public String scoutDetail(@PathVariable Integer id, HttpServletRequest req) {
         //1. 기업 정보 꺼내오기 (인증 체크)
         User sessionUser = (User) session.getAttribute("sessionUser");
         //2. 인재 명단에서 인재 클릭 시 이력서로 들어가지는 로직 짜기
-        Resume resume = (Resume) compQueryRepository.findResumeById(id);
+        Resume resume = (Resume) compService.getResumeDetail(id);
 
         req.setAttribute("sessionUser", sessionUser);
         req.setAttribute("resume", resume);
 
-        return "geust/resume/detailForm";
+        return "guest/resume/detailForm";
     }
 
     // DEL: getResumeList 삭제
 
-    // TODO: 서비스 만들기
     @GetMapping("/comp/mngForm")
     public String mngForm(HttpServletRequest req) {
-        // 채용 공고 목록 조회
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        List<Jobopen> temp = compQueryRepository.findJobopenById(sessionUser.getId());
-        List<JobopenResponse.DTO> jobopenList = temp.stream().map(jobopen -> new JobopenResponse.DTO(jobopen)).toList();
-
-        jobopenList.forEach(dto -> {
-            int count = applyJPARepository.countJobopenApplyById(dto.getId());
-            dto.setCount(count);
-        });
-
-//        req.setAttribute("count",countApplyList);
+        User sessionUser = (User) req.getSession().getAttribute("sessionUser");
+        List<JobopenResponse.DTO> jobopenList = compService.searchjobopenList(sessionUser.getId());
         req.setAttribute("jobopenList", jobopenList);
         return "comp/_myPage/mngForm";
     }
 
-
-    // TODO: 서비스 만들기
     @GetMapping("/comp/profileForm")
     public String profileForm(HttpServletRequest req) {
         User sessionUser = (User) session.getAttribute("sessionUser");
-
-        User compProfile = userJPARepository.findById(sessionUser.getId()).get();
+        User compProfile = compService.getCompanyProfile(sessionUser.getId());
         req.setAttribute("compProfile", compProfile);
         return "comp/_myPage/profileForm";
     }
