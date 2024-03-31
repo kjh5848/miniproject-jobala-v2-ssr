d19aa735de945928c42cb2e5d716ae77dd639ee1
vosw1 <vosw1@naver.com>
2024-03-31 16:48:49 +0900

    댓글 응답dto로 전환

--- src/main/java/com/example/jobala/board/BoardResponse.java
+++ src/main/java/com/example/jobala/board/BoardResponse.java
@@ -11,6 +11,58 @@ import java.util.List;
 
 public class BoardResponse {
 
+    // 글쓰기DTO
+    @Data
+    public static class BoardSaveDTO {
+        private Integer id;
+        private String title;
+        private String content;
+        private UserDTO user;
+
+        @Data
+        public class UserDTO {
+            private int id;
+
+            public UserDTO(User user) {
+                this.id = user.getId();
+            }
+        }
+
+
+        public BoardSaveDTO(Board board) {
+            this.id = board.getId();
+            this.title = board.getTitle();
+            this.content = board.getContent();
+            this.user = new UserDTO(board.getUser());
+        }
+    }
+
+    // 글 수정DTO
+    @Data
+    public static class BoardUpdateDTO {
+        private Integer id;
+        private String title;
+        private String content;
+        private UserDTO user;
+
+        @Data
+        public class UserDTO {
+            private int id;
+
+            public UserDTO(User user) {
+                this.id = user.getId();
+            }
+        }
+
+
+        public BoardUpdateDTO(Board board) {
+            this.id = board.getId();
+            this.title = board.getTitle();
+            this.content = board.getContent();
+            this.user = new UserDTO(board.getUser());
+        }
+    }
+
     @Data
     public static class DetailDTO {
         private int id;
@@ -67,29 +119,29 @@ public class BoardResponse {
     }
 
     // 자유게시판 상세페이지
-    @Data
-    public static class BoardDetailDTO {
-        private Integer id;
-        private String title;
-        private String content;
-        private Integer userId;
-        private String username;
-        private Boolean boardOwner;
-
-        // qlrm 맵핑은 순서를 맞춰주고 조인하는 컬럼만 생성자를 만들어서 초기화를 시켜준다.
-        public BoardDetailDTO(Integer id, String title, String content, Integer userId, String username) {
-            this.id = id;
-            this.title = title;
-            this.content = content;
-            this.userId = userId;
-            this.username = username;
-        }
-
-        // 보더의 주인여부 확인
-        public void isOwner(User sessionUser) {
-            if (sessionUser == null) boardOwner = false;
-            else boardOwner = sessionUser.getId() == userId;
-        }
-
-    }
+//    @Data
+//    public static class BoardDetailDTO {
+//        private Integer id;
+//        private String title;
+//        private String content;
+//        private Integer userId;
+//        private String username;
+//        private Boolean boardOwner;
+//
+//        // qlrm 맵핑은 순서를 맞춰주고 조인하는 컬럼만 생성자를 만들어서 초기화를 시켜준다.
+//        public BoardDetailDTO(Integer id, String title, String content, Integer userId, String username) {
+//            this.id = id;
+//            this.title = title;
+//            this.content = content;
+//            this.userId = userId;
+//            this.username = username;
+//        }
+//
+//        // 보더의 주인여부 확인
+//        public void isOwner(User sessionUser) {
+//            if (sessionUser == null) boardOwner = false;
+//            else boardOwner = sessionUser.getId() == userId;
+//        }
+//
+//    }
 }
